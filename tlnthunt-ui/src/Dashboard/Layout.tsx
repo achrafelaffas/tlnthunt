e72dcca4512e81cdf0105d55ff3a0dd600e0a61a
achraf <achrafelaffas@gmail.com>
2024-10-24 21:08:37 +0100

    Dashboard Pages

---
+++ tlnthunt-ui/src/Dashboard/Layout.tsx
@@ -0,0 +1,15 @@
+import Header from "@/components/Header";
+import { Outlet } from "react-router-dom";
+
+const Dashboard = () => {
+  return (
+    <div className="flex w-full flex-col dark:bg-black">
+      <Header />
+      <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4  p-4 md:gap-6 md:px-8 md:py-3">
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
+export default Dashboard;
